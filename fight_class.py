ce530fee2d9a36625742a0b31c66be845dedc4d2
Kumiko Motomiya <mana.love.hero@gmail.com>
2017-11-24 22:29:23 +0900

    クラスファイルに非表示機能追加

--- fight_class.py
+++ fight_class.py
@@ -5,3 +5,5 @@ class FightManager:
     def __init__(self):
         self.dialog = tkinter.Frame(width=820, height=434)
         self.dialog.place(x=10, y=10)
+        # 非表示
+        self.dialog.place_forget()
