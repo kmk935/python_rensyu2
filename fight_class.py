53c39b27a124e5d6c3f8c69a3643783b54916af2
Kumiko Motomiya <mana.love.hero@gmail.com>
2017-11-25 18:57:10 +0900

    クラスファイルに別画面用モジュール設定

--- fight_class.py
+++ fight_class.py
@@ -5,5 +5,13 @@ class FightManager:
     def __init__(self):
         self.dialog = tkinter.Frame(width=820, height=434)
         self.dialog.place(x=10, y=10)
+        canvas = tkinter.Canvas(self.dialog, width=820, height=434)
+        canvas.place(x=0, y=0)
+        canvas.create_rectangle(0, 0, 620, 434, fill="black")
+        # ボタン作成
+        winbutton = tkinter.Button(self.dialog, text="勝った")
+        winbutton.place(x=180, y=340)
+        losebutton = tkinter.Button(self.dialog, text="負けた")
+        losebutton.place(x=360, y=340)
         # 非表示
         self.dialog.place_forget()
